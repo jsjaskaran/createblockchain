9c72bb645a7574dbaa5f38eaca8bbadf9c021ad1
Jaskaran <jaskarans13@gmail.com>
2018-07-21 08:51:40 +0530

    Creating a blockchain class completed

--- blockchain.py
+++ blockchain.py
@@ -30,3 +30,48 @@ class Blockchain:
 	# return last block of chain
 	def get_previous_block(self):
 		return self.chain[-1]
+
+	def proof_of_work(self, prev_proof):
+		new_proof = 1 # increment by 1 to find the right proof (solve by trial-error approach)
+		check_proof = False
+
+		while check_proof is False:
+			# problem that miners have to solve, can make operation more complex for challenging problem
+			hash_operation = hashlib.sha256(str(new_proof ** 2 - prev_proof ** 2).encode()).hexdigest()
+			if hash_operation[:4] == '0000':
+				check_proof = True
+			else:
+				new_proof += 1
+
+		return new_proof
+
+	# create a hash of the block
+	def hash(self, block):
+		encoded_block = json.dumps(block, sort_keys = True).encode()
+		return hashlib.sha256(encoded_block).hexdigest()
+
+	# check if the chain is valid
+	def is_chain_valid(self, chain):
+		previous_block = chain[0]
+		block_index = 1
+
+		while block_index < len(chain):
+			block = chain[block_index]
+			# previous hash of this block is equal to hash of previous block
+			if block['previous_hash'] != self.hash(previous_block):
+				return False
+
+			# check for proof of work
+			previous_proof = previous_block['proof']
+			proof = block['proof']
+
+			hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+			if hash_operation[:4] != '0000':
+				return False
+
+			previous_block = block
+			block_index += 1
+
+		return True
+
+
